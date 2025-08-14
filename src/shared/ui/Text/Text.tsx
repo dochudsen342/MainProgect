5214b5455e9741cbcd2db4e2e177c6c0acf9c556
dochudsen342 <dmitriy_kolesnikov_2003@list.ru>
2025-08-14 08:15:30 +0300

    fix commit

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -38,11 +38,8 @@ const Text = ({ className, title, text, theme, aligin,size = TextSize.M }: TextP
            
     
     return (
-<<<<<<< HEAD
-        <div className={classNames(cl.Text, {}, [className || ''])}>
-=======
         <div className={classNames(cl.Text, mods, [className || ''])}>
->>>>>>> 8416dd9e1dfcf3d14a232353dc9ff609e99f6d75
+
             {title && <p className={cl.title}>{title}</p>}
             {text && <p className={cl.subtitle}>{text}</p>}
         </div>
