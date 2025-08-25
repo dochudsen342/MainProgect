76ba54911b6ea7cb59beb9bf409c14c210c33b4f
dochudsen342 <dmitriy_kolesnikov_2003@list.ru>
2025-08-25 20:32:43 +0300

    add storybook

---
+++ src/shared/lib/decorators/theme.decorator.tsx
@@ -0,0 +1,21 @@
+import { Theme } from "app/providers/ThemeProvider";
+
+const ThemeDecorator = (theme:Theme) => (Story:any) => {
+  
+  return (
+    <div 
+      className={`app ${theme}`}
+      style={{
+        width:'100vh',
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+     <Story/>
+    </div>
+  );
+};
+
+export default ThemeDecorator
---
+++ src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react-webpack5';
+
+import { fn } from 'storybook/test';
+import Button, { ThemeButton } from './Button';
+import { StoryAnnotations } from 'storybook/internal/csf';
+import ThemeDecorator from 'shared/lib/decorators/theme.decorator';
+import { Theme } from 'app/providers/ThemeProvider';
+
+const meta = {
+  title: 'shared/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { onClick: fn() },
+   
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const Primary: Story = {
+  args: {
+    children: 'Text',
+  },
+  decorators:[ThemeDecorator(Theme.DARK)]
+}
+export const Outline: Story = {
+  args: {
+    children: 'Text',
+    theme:ThemeButton.OUTLINE
+  },
+ decorators:[ThemeDecorator(Theme.LIGHT)]
+};
+export const OutlineDark: Story = {
+  args: {
+    children: 'Text',
+    theme:ThemeButton.OUTLINE
+  },
+ decorators:[ThemeDecorator(Theme.DARK)]
+};
+
